73c1f76ef87971a441b3d7e18e8d6219122e7e81
eric bash on swiss army <ericmaina.dev@gmail.com>
2024-05-31 00:03:41 +0300

    fixing cors problem

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+const cors = require('cors');
+
+// Add CORS middleware
+server.use(cors());
 
 server.use(middlewares);
 server.use(router);
